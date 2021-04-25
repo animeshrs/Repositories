0645429a5098713d372bdfed27df2268e9caa1e3
animesh <william.animesh@gmail.com>
2021-04-25 01:17:54 +0100

    end of section 15

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -27,6 +27,7 @@ namespace API.Controllers
             return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
         }
 
+        [AllowAnonymous]
         [Authorize(Policy = "IsActivityHost")]
         [HttpPut("{id}")]
         public async Task<IActionResult> EditActivity(Guid id, Activity activity)
--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -1,5 +1,7 @@
 /** @format */
 
+import { Profile } from "./profile";
+
 export interface Activity {
 	id: string;
 	title: string;
@@ -8,4 +10,38 @@ export interface Activity {
 	category: string;
 	city: string;
 	venue: string;
+	hostUsername: string;
+	isCancelled: boolean;
+	isGoing: boolean;
+	isHost: boolean;
+	host?: Profile;
+	attendees: Profile[];
+}
+
+export class Activity implements Activity {
+	constructor(init?: ActivityFormValues) {
+		Object.assign(this, init);
+	}
+}
+
+export class ActivityFormValues {
+	id?: string = undefined;
+	title: string = "";
+	date: Date | null = null;
+	description: string = "";
+	category: string = "";
+	city: string = "";
+	venue: string = "";
+
+	constructor(activity?: ActivityFormValues) {
+		if (activity) {
+			this.id = activity.id;
+			this.category = activity.category;
+			this.city = activity.city;
+			this.date = activity.date;
+			this.description = activity.description;
+			this.title = activity.title;
+			this.venue = activity.venue;
+		}
+	}
 }
